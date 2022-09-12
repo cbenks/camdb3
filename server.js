9e73c780d7f9a3be025d39e7b9645dda8b64a108
Cory <cjbenicak@gmail.com>
2022-09-12 10:15:14 -0400

    added scripts, made gitignore and env files

--- server.js
+++ server.js
@@ -1,8 +1,15 @@
 const express = require('express')
 const cors = require('cors')
-const PORT = process.env.PORT || 3001
 
 const app = express()
+
+const AppRouter = require('./routes/AppRouter')
+
+const PORT = process.env.PORT || 3001
+
+app.use(cors())
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
 
 app.listen(PORT, () => {
   console.log(`Express server listening on port ${PORT}`)
