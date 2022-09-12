513a8af3827f557d72d0200f2a5eaca287a8560f
Cory <cjbenicak@gmail.com>
2022-09-12 10:46:00 -0400

    installed dependencies body parser bcrypt jwt dotenv

--- server.js
+++ server.js
@@ -11,6 +11,8 @@ app.use(cors())
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 
+app.get('/', (req, res) => res.json({ message: 'Server Works' }))
+// app.use('/api', AppRouter)
 app.listen(PORT, () => {
   console.log(`Express server listening on port ${PORT}`)
 })
